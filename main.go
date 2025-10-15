20d44db6038aa6c85bc3e730626bc49cb2511649
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2025-10-15 20:43:28 +0700

    feat: starter

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct{}
+
+func (m *model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *model) Update(teaMsg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := teaMsg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func newModel() *model {
+	return &model{}
+}
+
+func (m *model) View() string {
+	return "Hello World!"
+}
+
+func main() {
+	m := newModel()
+
+	p := tea.NewProgram(m)
+
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
