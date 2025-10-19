b790817ffebd27b2be545186e0caea3fb6b89d89
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2025-10-19 20:08:52 +0700

    feat: sendMessage cmd

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
 	openai "github.com/openai/openai-go/v3"
@@ -15,17 +17,36 @@ type model struct {
 	textarea     textarea.Model
 	client       openai.Client
 	chatModel    openai.ChatModel
+	spinner      spinner.Model
 	reply        string
+	loading      bool
+}
+
+type apiResponseMsg struct {
+	reply string
+	param openai.ChatCompletionMessageParamUnion
+}
+
+type apiErrorMsg struct {
+	err error
+}
+
+func (e *apiErrorMsg) Error() string {
+	return e.err.Error()
 }
 
 func (m *model) Init() tea.Cmd {
-	return nil
+	return m.textarea.Cursor.BlinkCmd()
 }
 
 func (m *model) Update(teaMsg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 
 	if cmd := m.updateTextArea(teaMsg); cmd != nil {
+		cmds = append(cmds, cmd)
+	}
+
+	if cmd := m.updateSpinner(teaMsg); cmd != nil {
 		cmds = append(cmds, cmd)
 	}
 
@@ -37,19 +58,13 @@ func (m *model) Update(teaMsg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
 		case tea.KeyEnter:
-			input := m.textarea.Value()
-			m.conversation = append(m.conversation, openai.UserMessage(input))
-			resp, _ := m.client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
-				Model:    m.chatModel,
-				Messages: m.conversation,
-			})
-
-			message := resp.Choices[0].Message
-
-			m.conversation = append(m.conversation, message.ToParam())
-			m.reply = message.Content
+			m.reply = fmt.Sprintf("%s %s", m.spinner.View(), "Thinking...")
+			m.loading = true
+			cmds = append(cmds, m.spinner.Tick, m.sendMessage(ctx))
 		}
-
+	case apiResponseMsg:
+		m.loading = false
+		m.reply = fmt.Sprintf("%s %s", "Response: ", msg.reply)
 	}
 	return m, tea.Batch(cmds...)
 }
@@ -61,8 +76,45 @@ func (m *model) updateTextArea(msg tea.Msg) tea.Cmd {
 	return cmd
 }
 
+func (m *model) updateSpinner(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+	m.spinner, cmd = m.spinner.Update(msg)
+
+	if _, ok := msg.(spinner.TickMsg); ok && m.loading {
+		m.reply = fmt.Sprintf("%s %s", m.spinner.View(), "Thinking...") // update reply with new spinner frame
+	}
+
+	return cmd
+}
+
 func (m *model) View() string {
 	return fmt.Sprintf("%s\n\n%s", m.textarea.View(), m.reply)
+}
+
+func (m *model) sendMessage(ctx context.Context) tea.Cmd {
+	return func() tea.Msg {
+		input := m.textarea.Value()
+		input = strings.TrimSpace(input)
+
+		m.conversation = append(m.conversation, openai.UserMessage(input))
+		resp, err := m.client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+			Model:    m.chatModel,
+			Messages: m.conversation,
+		})
+
+		if err != nil {
+			return &apiErrorMsg{err: err}
+		}
+
+		message := resp.Choices[0].Message
+
+		m.conversation = append(m.conversation, message.ToParam())
+		m.reply = message.Content
+		return apiResponseMsg{
+			reply: message.Content,
+			param: message.ToParam(),
+		}
+	}
 }
 
 func newModel() *model {
@@ -74,10 +126,14 @@ func newModel() *model {
 
 	client := openai.NewClient()
 
+	sp := spinner.New()
+	sp.Spinner = spinner.Points
+
 	return &model{
 		textarea:  ta,
 		client:    client,
 		chatModel: openai.ChatModelGPT3_5Turbo,
+		spinner:   sp,
 		conversation: []openai.ChatCompletionMessageParamUnion{
 			openai.SystemMessage("You are a sarcastic roast master who specializes in roasting developers. Be witty, funny, mean, and savage, but never offensive. Your job is to roast any tech-related confession the user gives you. Be concise and to the point."),
 		},
