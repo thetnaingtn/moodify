f9b7312598e2273182d88ed3bc20bbb0d79b79d9
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2025-10-19 17:13:14 +0700

    feat: add textarea

--- main.go
+++ main.go
@@ -1,40 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct{}
+type model struct {
+	conversation string
+	textarea     textarea.Model
+}
 
 func (m *model) Init() tea.Cmd {
 	return nil
 }
 
 func (m *model) Update(teaMsg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	if cmd := m.updateTextArea(teaMsg); cmd != nil {
+		cmds = append(cmds, cmd)
+	}
+
 	switch msg := teaMsg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
+		case tea.KeyEnter:
+			input := m.textarea.Value()
+			m.conversation = input
 		}
+
 	}
-	return m, nil
+	return m, tea.Batch(cmds...)
 }
 
-func newModel() *model {
-	return &model{}
+func (m *model) updateTextArea(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+	m.textarea, cmd = m.textarea.Update(msg)
+
+	return cmd
 }
 
 func (m *model) View() string {
-	return "Hello World!"
+	return fmt.Sprintf("%s\n\n%s", m.textarea.View(), m.conversation)
 }
 
 func main() {
-	m := newModel()
+	ta := textarea.New()
+	ta.ShowLineNumbers = false
+	ta.Focus()
+	ta.Placeholder = "Send a message..."
+	ta.SetHeight(3)
 
-	p := tea.NewProgram(m)
+	p := tea.NewProgram(&model{
+		textarea: ta,
+	})
 
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
